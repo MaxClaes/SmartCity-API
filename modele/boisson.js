a0e04fe222f670173e6464c48e7ec934a4f26bd0
Maxime Claes <claesma25@gmail.com>
2020-11-18 22:30:25 +0100

    MAJ fichier boisson dans modele

--- modele/boisson.js
+++ modele/boisson.js
@@ -1,21 +1,38 @@
 module.exports.createBoisson = async (client, label, prcAlcool, quantite) => {
-    const nbSignalementInitial = 0;
+    const nbSignalementInitial = 0; //Faire dans le fichier sql de création de table pour que cette variable soit initialisée à 0
 
     return await client.query(`
         INSERT INTO boisson(label, prcAlcool, quantite, nbSignalements) 
-        VALUES ($1, $2, $3, $4)`, [label, prcAlcool, quantite, nbSignalementInitial]
+        VALUES ($1, $2, $3, $4);
+        `, [label, prcAlcool, quantite, nbSignalementInitial]
     );
 };
 
-module.exports.updateBoisson = async (client, label, prcAlcool, quantite, id) => {
+module.exports.updateBoisson = async (client, label, prcAlcool, quantite, nbSignalements, id) => {
      return await client.query(`
-        UPDATE boisson SET label = $1, prcAlcool = $2, quantite = $3
-        WHERE id = $4
-   `, [label, prcAlcool, quantite, id]);
+        UPDATE boisson SET label = $1, prcAlcool = $2, quantite = $3, nbSignalements = $4
+        WHERE id = $5;
+        `, [label, prcAlcool, quantite, nbSignalements, id]
+     );
 };
 
 module.exports.getBoissonByName = async (client, label) => {
     return await client.query(`
         SELECT * FROM boisson WHERE label = $1;
-    `, [label]);
+        `, [label]
+    );
 };
+
+module.exports.getBoissonByUserId = async (client, id) => {
+    return await client.query(`
+        SELECT * FROM boisson WHERE createBy = $1;
+        `, [id]
+    );
+};
+
+module.exports.deleteBoisson = async (client, id) => {
+    return await client.query(`
+        DELETE from boisson WHERE id = $1;
+        `, [id]
+    );
+}
