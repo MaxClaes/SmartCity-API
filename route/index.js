9559d62101b923de2be1893848ca51ea1252a3d6
Maxime Claes <claesma25@gmail.com>
2020-11-25 16:43:38 +0100

    Refactoring

--- route/index.js
+++ route/index.js
@@ -1,12 +1,10 @@
 const UserRouter = require('./user');
 const DrinkRouter = require('./drink');
-const ReportRouter = require('./report');
 const BandRouter = require('./band');
 const router = require("express").Router();
 
 router.use("/user", UserRouter);
 router.use("/drink", DrinkRouter);
-router.use("/report", ReportRouter);
 router.use("/band", BandRouter);
 
 module.exports = router;
