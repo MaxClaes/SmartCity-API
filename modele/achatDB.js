9bce5287b2f67eb6c3c4eaff923388d9a6e35663
Maxime Claes <claesma25@gmail.com>
2020-11-25 13:01:48 +0100

    Suppression fichiers inutiles

--- modele/achatDB.js
+++
@@ -1,6 +0,0 @@
-// module.exports.insertAchat = async (client, id_client, id_produit, quantite, date = new Date().toISOString()) => {
-//     return await client.query(`
-//         INSERT INTO achat(id_client, id_produit, quantite, date) VALUES
-//         ($1, $2, $3, $4)`, [id_client, id_produit, quantite, date]
-//     );
-// }
--- modele/managerDB.js
+++
@@ -1,5 +0,0 @@
-// module.exports.getManager = async (client, email) => {
-//     return await client.query(`
-//         SELECT * FROM manager WHERE email = $1;
-//     `, [email]);
-// }
