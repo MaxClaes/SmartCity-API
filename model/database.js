a6ce5ed54e11cd21bcb5b2cfc547d585161ea4da
Maxime Claes <claesma25@gmail.com>
2020-12-18 12:50:32 +0100

    Nettoyage fichier model database

--- model/database.js
+++ model/database.js
@@ -4,16 +4,18 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 // const username = process.env.DB_USER;
-// //maxime_claes@postgresqlclaesma.postgres.database.azure.com
 // const dbPassword = process.env.DB_PASSWORD;
 // const dbHost = process.env.DB_HOST;
 // const dbDatabase = process.env.DB_DATABASE
 // const dbPort = process.env.DB_PORT;
-
+//
 // const connectionString = `postgresql://${username}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}?ssl=true`
+//
+// const pool = new Pool({
+//     connectionString
+// });
 
 const pool = new Pool({
-    // connectionString
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
