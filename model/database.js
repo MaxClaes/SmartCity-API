f493b0dddc2e361a054e8b15c26546d444f452fd
Maxime Claes <claesma25@gmail.com>
2020-12-19 17:06:10 +0100

    Modification azure

--- model/database.js
+++ model/database.js
@@ -6,10 +6,10 @@ const Pool = pg.Pool;
 const username = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbHost = process.env.DB_HOST;
-const dbDatabase = process.env.DB_DATABASE
+const dbDatabase = process.env.DB_DATABASE;
 const dbPort = process.env.DB_PORT;
 
-const connectionString = `postgresql://${username}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}?ssl=true`
+const connectionString = `postgresql://${username}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}?ssl=true`;
 
 const pool = new Pool({
     connectionString
